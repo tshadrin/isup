19123ec753aea09eeb8e4ebd052a3530f06c9383
tshadrin <tshadrin@gmail.com>
2019-07-31 02:12:59 +0300

    Исправлено имя тега для интервала времени

--- assets/js/sberbank.js
+++ assets/js/sberbank.js
@@ -3,7 +3,7 @@ require('jquery-date-range-picker/src/daterangepicker.scss');
 require('jquery-date-range-picker');
 
 jQuery(document).ready(function() {
-    jQuery('#payment_filter_form_reg_date_interval').dateRangePicker({
+    jQuery('#form_interval').dateRangePicker({
         startOfWeek: 'monday',
         format: 'DD-MM-YYYY',
         language: 'ru',
