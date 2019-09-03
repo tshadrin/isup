c1699b18be77d6b61e81d5b2f4155df9e5060a1a
sham <sham@istranet.ru>
2019-09-03 17:07:50 +0300

    Переработан код показа карточки клиента.

--- src/Service/UTM5/CallRegister/Handler.php
+++ src/Service/UTM5/CallRegister/Handler.php
@@ -5,14 +5,18 @@ namespace App\Service\UTM5\CallRegister;
 
 
 
+use App\Entity\User\User;
 use App\ReadModel\UTM5\UserFetcher;
 use App\Repository\UserRepository;
 use phpcent\Client;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
+use Symfony\Component\Templating\EngineInterface;
 
 class Handler
 {
+    const CALL_CHANNEL_NAME = 'calls';
     /**
      * @var Client
      */
@@ -33,43 +37,80 @@ class Handler
      * @var UserRepository
      */
     private $userRepository;
+    /**
+     * @var EngineInterface
+     */
+    private $templating;
+    private $command;
+    private $userFields;
 
     public function __construct(Client $centrifugo,
                                 UserFetcher $userFetcher,
                                 UrlGeneratorInterface $urlGenerator,
                                 TokenStorageInterface $tokenStorage,
-                                UserRepository $userRepository)
+                                UserRepository $userRepository, EngineInterface $templating)
     {
         $this->centrifugo = $centrifugo;
+        $this->centrifugo->setSafety(false);
         $this->userFetcher = $userFetcher;
         $this->urlGenerator = $urlGenerator;
         $this->tokenStorage = $tokenStorage;
         $this->userRepository = $userRepository;
+        $this->templating = $templating;
     }
 
     public function handle(Command $command): void
     {
-        $this->centrifugo->setSafety(false);
+        $this->command = $command;
+        $user = $this->getUser();
+
         try {
-            $user = $this->userRepository->findByInternalNumber($command->operatorNumber);
-            if(!is_null($user)) {
-                $userFields = $this->userFetcher->getUserByPhone(mb_substr($command->callerNumber, 2));
-                $url = $this->urlGenerator->generate("search.by.data", ["value" => $userFields['id'], "type" => 'id']);
-                $channel = "calls#".$user->getId();
-                if(!empty($userFields['flat_number']))
-                    $userFields['actual_address'] .= " - {$userFields['flat_number']}";
-                $this->centrifugo->publish($channel, [
-                    'message'=>"
-<div style=\"font-size:1rem;\"><p>{$command->callerNumber}</p>
-<p>{$userFields['full_name']}<br>
-{$userFields['actual_address']}</p>
-<a class=\" bg-success\" href=\"{$url}\">Отрыть карточку клиента</a><br>
-<a class=\" bg-info\" target=\"_blank\" href=\"https://istranet.pro/crm/deal/list/?apply_filter=Y&with_preset=Y&FIND={$command->callerNumber}\">Открыть карточку в bitrix24</a>
-</div>",
-                ]);
-            }
+            $this->userFields = $this->getUTM5UserData();
+            $this->publish($user->getId(), $this->renderUserCard());
         } catch(\DomainException $e) {
-            $this->centrifugo->publish('calls', ['message'=> "<br><a class=\" bg-info\" target=\"_blank\" href=\"https://istranet.pro/crm/deal/list/?apply_filter=Y&with_preset=Y&FIND={$command->callerNumber}\">Открыть карточку в bitrix24</a>"]);
+            $this->publish($user->getId(), $this->renderNoUserCard());
         }
+    }
+
+    private function getUser(): User
+    {
+        $user = $this->userRepository->findByInternalNumber($this->command->operatorNumber);
+        if (is_null($user)) {
+            throw new NotFoundHttpException("User with internal number {$this->command->operatorNumber} not found.");
+        }
+        return $user;
+    }
+
+    private function getUTM5UserData(): array
+    {
+        $data = $this->userFetcher->getUserByPhone(mb_substr($this->command->callerNumber, 2));
+        if(!empty($data['flat_number']))
+            $data['actual_address'] .= " - {$data['flat_number']}";
+        return $data;
+    }
+
+    private function publish(int $userId, string $message): void
+    {
+        $this->centrifugo->publish(self::CALL_CHANNEL_NAME. "#" . $userId, ['message'=> $message]);
+    }
+
+    private function renderUserCard(): string
+    {
+        return $this->templating->render("widget/register-call/user-card.html.twig", [
+                'callerNumber' => $this->command->callerNumber,
+                'userAddress' => $this->userFields['actual_address'],
+                'userFullName' => $this->userFields['full_name'],
+                'monCardUrl' => $this->urlGenerator->generate("search.by.data", ["value" => $this->userFields['id'], "type" => 'id']),
+                'bitrixCardUrl' => "https://istranet.pro/crm/deal/list/?apply_filter=Y&with_preset=Y&FIND={{$this->command->callerNumber}}",
+            ]
+        );
+    }
+
+    private function renderNoUserCard(): string
+    {
+        return $this->templating->render("widget/register-call/no-user-card.html.twig",  [
+            'callerNumber' => $this->command->callerNumber,
+            'bitrixCardUrl' => "https://istranet.pro/crm/deal/list/?apply_filter=Y&with_preset=Y&FIND={{$this->command->callerNumber}}"
+        ]);
     }
 }
