9a08aa275d3a37486b5301061c88d97e147bd9e0
sham <sham@istranet.ru>
2019-09-03 17:18:15 +0300

    Добавлен id клиента в карточку и  рекомендуемый платеж

--- src/Service/UTM5/CallRegister/Handler.php
+++ src/Service/UTM5/CallRegister/Handler.php
@@ -8,6 +8,7 @@ namespace App\Service\UTM5\CallRegister;
 use App\Entity\User\User;
 use App\ReadModel\UTM5\UserFetcher;
 use App\Repository\UserRepository;
+use App\Service\UTM5\URFAService;
 use phpcent\Client;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
@@ -43,20 +44,27 @@ class Handler
     private $templating;
     private $command;
     private $userFields;
+    /**
+     * @var URFAService
+     */
+    private $URFAService;
 
     public function __construct(Client $centrifugo,
                                 UserFetcher $userFetcher,
                                 UrlGeneratorInterface $urlGenerator,
                                 TokenStorageInterface $tokenStorage,
-                                UserRepository $userRepository, EngineInterface $templating)
+                                UserRepository $userRepository,
+                                EngineInterface $templating,
+                                URFAService $URFAService)
     {
         $this->centrifugo = $centrifugo;
-        $this->centrifugo->setSafety(false);
+        $this->centrifugo->setSafety(false); //нужно только для теста (
         $this->userFetcher = $userFetcher;
         $this->urlGenerator = $urlGenerator;
         $this->tokenStorage = $tokenStorage;
         $this->userRepository = $userRepository;
         $this->templating = $templating;
+        $this->URFAService = $URFAService;
     }
 
     public function handle(Command $command): void
@@ -98,8 +106,10 @@ class Handler
     {
         return $this->templating->render("widget/register-call/user-card.html.twig", [
                 'callerNumber' => $this->command->callerNumber,
+                'userId' => $this->userFields['id'],
                 'userAddress' => $this->userFields['actual_address'],
                 'userFullName' => $this->userFields['full_name'],
+                'userRequirementPayment' => $this->URFAService->getRequirementPaymentForUser($this->userFields['basic_account']),
                 'monCardUrl' => $this->urlGenerator->generate("search.by.data", ["value" => $this->userFields['id'], "type" => 'id']),
                 'bitrixCardUrl' => "https://istranet.pro/crm/deal/list/?apply_filter=Y&with_preset=Y&FIND={{$this->command->callerNumber}}",
             ]
